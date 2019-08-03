62b79e64b712fac669c15facecc5dc02c0a556f0
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-03 15:06:13 -0700

    add click handler to number buttons

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,10 +1,12 @@
-import React from "react"
+import React, { useContext } from "react"
+import { ActionsCtx } from '../../../App'
 
 export default function NumberButton(props) {
+  const { dispatch, append } = useContext(ActionsCtx)
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button>{props.content}</button>
+      <button onClick={() => dispatch( append(props.value) ) }>{props.char}</button>
     </>
   )
 }
