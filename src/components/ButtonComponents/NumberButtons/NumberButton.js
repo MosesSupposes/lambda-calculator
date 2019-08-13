ea04f6e94eda0bcda02d98f0d282265faa214ebc
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-13 13:01:24 -0700

    style NumberButtons

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,13 +1,27 @@
 import React, { useContext } from "react"
 import { ActionsCtx } from '../../../App'
 
+
+const styles = {
+  numberBtn: {
+    borderRadius: "100%",
+    border: "1px solid black",
+    backgroundColor: "navy",
+    color: "white",
+    fontWeight: "bold",
+    padding: ".75rem",
+    margin: ".5rem",
+    boxShadow: "1px 1px black",
+  }
+}
+
 export default function NumberButton(props) {
   const { appendCalc } = useContext(ActionsCtx)
 
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button onClick={() => appendCalc(props.value)}>{props.char}</button>
+      <button style={styles.numberBtn} onClick={() => appendCalc(props.value)}>{props.char}</button>
     </>
   )
 }
