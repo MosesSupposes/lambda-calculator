eb5badc89c5f45a51cf2ab7fe6f0a0b9087dd722
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-13 12:37:57 -0700

    inline logo and display styles in JS

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,17 @@
 import React from "react"
 
+
+const styles = {
+  display: {
+    textAlign: "end",
+    background: "rgb(75,75,75)",
+    padding: "1rem 2rem",
+    borderRadius: "25px",
+    color: "white",
+    maxWidth: "600px"
+  }
+}
+
 export default function Display(props) {
-  return <div className="display">{props.children}</div>
+  return <div style={styles.display} className="display">{props.children}</div>
 }
--- src/components/DisplayComponents/Logo.js
+++ src/components/DisplayComponents/Logo.js
@@ -2,9 +2,17 @@ import React from "react"
 
 import logo from "../../Img/Lambda_Logo_white.png"
 
+
+const styles = {
+  logoContainer:  {
+    paddingLeft: "5rem",
+    alignSelf: "flex-start",
+  }
+}
+
 const Logo = () => {
   return (
-    <div className="logo-container">
+    <div style={styles.logoContainer} className="logo-container">
       <img className="logo" src={logo} alt="Lambda's logo" />
     </div>
   )
