51959bb3b08c7ff4a419bf5013da841b332bb6bc
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-13 13:02:21 -0700

    style OperatorButtons

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,13 +1,27 @@
 import React, { useContext } from "react"
 import { ActionsCtx } from '../../../App'
 
+
+const styles = {
+  operatorBtn: {
+    borderRadius: "100%",
+    border: "1px solid black",
+    backgroundColor: "teal",
+    color: "white",
+    fontWeight: "bold",
+    padding: ".75rem",
+    margin: ".5rem",
+    boxShadow: "1px 1px black",
+  }
+}
+
 export default function OperatorButton(props) {
   const { appendCalc, calculate } = useContext(ActionsCtx)
 
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button onClick={(e) => {
+      <button style={styles.operatorBtn} onClick={(e) => {
         (props.char === "=") 
           ? calculate()
           : appendCalc(props.value) 
